30b926bd494fe62242e0d127ea0e96a693895feb
Angela Odhiambo <angelaodhiambo87@gmail.com>
2023-03-29 17:25:42 +0300

    Adding save button and previous entries

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-//create a new function called countEl
-let countEl = document.getElementById("count-el") // pass in arguments
-//console.log it to check for any errors
-console.log(countEl)
-
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
